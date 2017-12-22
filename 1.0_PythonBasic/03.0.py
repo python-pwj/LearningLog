126ddb5b7af60a3255e49121ad4fcd327741c912
python-pwj <34359715+python-pwj@users.noreply.github.com>
2017-12-22 21:58:26 +0800

    Create 03.0.py

---
+++ 1.0_PythonBasic/03.0.py
@@ -0,0 +1,16 @@
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+print(len(classmates))
+#输出第一个
+print(classmates[0])
+#输出倒数第二个
+print(classmates[-2])
+#末尾追加方法
+classmates.append('Adam')
+print(classmates)
+#插入
+classmates.insert(1, 'Jack') #第二个位置插入Jack
+print(classmates)
+#删除
+classmates.pop()
+print(classmates)
